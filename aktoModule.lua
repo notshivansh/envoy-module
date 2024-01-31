99f4e5f53186abe9df9a8471ec4687c90b1176c6
notshivansh <shivansh@akto.io>
2024-01-31 12:29:52 +0530

    update aktomodule

--- aktoModule.lua
+++ aktoModule.lua
@@ -1,4 +1,3 @@
-local cjson = require 'cjson'
 
 local friendlyHttpStatus={['200']='OK',['201']='Created',['202']='Accepted',['203']='Non-AuthoritativeInformation',['204']='NoContent',['205']='ResetContent',['206']='PartialContent',['300']='MultipleChoices',['301']='MovedPermanently',['302']='Found',['303']='SeeOther',['304']='NotModified',['305']='UseProxy',['306']='Unused',['307']='TemporaryRedirect',['400']='BadRequest',['401']='Unauthorized',['402']='PaymentRequired',['403']='Forbidden',['404']='NotFound',['405']='MethodNotAllowed',['406']='NotAcceptable',['407']='ProxyAuthenticationRequired',['408']='RequestTimeout',['409']='Conflict',['410']='Gone',['411']='LengthRequired',['412']='PreconditionRequired',['413']='RequestEntryTooLarge',['414']='Request-URITooLong',['415']='UnsupportedMediaType',['416']='RequestedRangeNotSatisfiable',['417']='ExpectationFailed',['418']='I\'mateapot',['429']='TooManyRequests',['500']='InternalServerError',['501']='NotImplemented',['502']='BadGateway',['503']='ServiceUnavailable',['504']='GatewayTimeout',['505']='HTTPVersionNotSupported'}
 
@@ -30,7 +29,9 @@ end
 local function producer(message)
     local config = require 'rdkafka.config'.create()
     local kafkaServer = os.getenv("AKTO_KAFKA_IP")
-    print("traffic res: ", table_to_string(message))
+    print("kafka server: ", kafkaServer)
+    local messageString = table_to_string(message)
+    print("traffic res: ", messageString)
     if kafkaServer~=nil then
         config["statistics.interval.ms"] = "100"
         config["bootstrap.servers"] = kafkaServer
@@ -44,7 +45,7 @@ local function producer(message)
         local topic = require 'rdkafka.topic'.create(producer, "akto.api.logs", topic_config)
 
         local KAFKA_PARTITION_UA = -1
-        producer:produce(topic, KAFKA_PARTITION_UA, cjson.encode(table_to_string(message)))
+        producer:produce(topic, KAFKA_PARTITION_UA, messageString)
 
         while producer:outq_len() ~= 0 do
             producer:poll(10)
